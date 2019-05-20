7779494df638f79c3ac8a1964cff7752c0ec7790
katerinaSo <katya.solstad@gmail.com>
2019-05-20 14:54:20 -0400

    generating random cards, layout improved to center

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react'
-import {genImage} from './helper'
-import {pokemons} from '../src/assets/pokemons'
-import './App.css'
-
+import React, { Component } from "react";
+import { genImage } from "./helper";
+import "./App.css";
 
 export class Pokecard extends Component {
-    
-    
   render() {
-   const ids=Object.keys(pokemons)
-   const arrIds=ids.map((id)=>{return parseInt(id)}) 
-   const genIndex=()=>{return Math.floor(Math.random()*(arrIds.length))}
-   let newID=arrIds[genIndex()]
-   let newData= pokemons[newID]
-   let newPower= newData.defense+newData.attack
-   let newName=newData.name
-   
-   
+    const { newID, newData } = this.props;
+
     return (
       <div className="Pokecard">
-       <div>
-          <h1 className="Pokecard-title">{newName}</h1>
-          <p ><img className="Pokecard-img" src={genImage(newID)} alt={`pokemon ${newData.name}`}/></p>
-          <p className="Pokecard-type">type: {newData.type}</p>
-          <p className="Pokecard-power">power: {newPower}</p>
-          </div>
-          
-       </div>
-    )
+        <h1 className="Pokecard-title">{newData.name}</h1>
+        <p>
+          <img
+            className="Pokecard-img"
+            src={genImage(newID)}
+            alt={`pokemon ${newData.name}`}
+          />
+        </p>
+        <p className="Pokecard-type">type: {newData.type}</p>
+        <p className="Pokecard-power">power: {newData.attack+newData.defense}</p>
+      </div>
+    );
   }
 }
 
-export default Pokecard
-
+export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
 import Card from "./Pokecard";
-import "./App.css"
+import { pokemons } from "../src/assets/pokemons";
+import "./App.css";
 
 export class Pokedex extends Component {
   render() {
-    return <div >
-        <div className='Pokedex-hand'>
-        <Card/>
-        <Card/>
-        <Card/>
-
+    const ids = Object.keys(pokemons);
+    const arrIds = ids.map(id => {
+      return parseInt(id);
+    });
+    const genIndex = () => {
+      return Math.floor(Math.random() * arrIds.length);
+    };
+    return (
+      <div>
+        <div className="Pokedex-hand">
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
         </div>
-        <div className='Pokedex-hand'>
-        <Card/>
-        <Card/>
-        <Card/>
-
+        <div className="Pokedex-hand">
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
+          <Card
+            newID={arrIds[genIndex()]}
+            newData={pokemons[arrIds[genIndex()]]}
+          />
         </div>
-        
-        </div>
+      </div>
+    );
   }
 }
 
